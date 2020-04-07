f4dbd12e9490c62c264ccce6171c5f8f71ac0d43
Adam <gienappa@gmail.com>
2020-04-07 14:14:25 -0700

    radixSort complete

--- sorting algorithms/radixSort.js
+++ sorting algorithms/radixSort.js
@@ -1,5 +1,20 @@
+function radixSort (array, digit = 0) {
+  const buckets = [[],[],[],[],[],[],[],[],[]];
 
-
+  for (let num of array) {
+    let idx = getDigit(num, digit);
+    buckets[idx].push(num);
+  }
+  if (buckets[0].length === array.length) {
+    return buckets[0];
+  } else {
+    let newArray = [];
+    for (let i in buckets) {
+      newArray = newArray.concat(buckets[i]);
+    }
+    return radixSort(newArray, digit + 1);
+  }
+}
 
 function getDigit(num, place) {
   let div = num / (10 ** (place + 1));
