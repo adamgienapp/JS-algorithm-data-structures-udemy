480f22e27b11f7a76303b579506ce12558c9f58d
Adam <gienappa@gmail.com>
2020-04-13 13:29:52 -0700

    doublyLinkedList - complete thru get

---
+++ data-structures/doublyLinkedList.js
@@ -0,0 +1,101 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    var newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (this.length < 1) {
+      console.log("There's nothing to remove!");
+      return undefined;
+    }
+    var oldTail = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      oldTail.prev = null;
+    }
+    this.length--;
+    return oldTail;
+  }
+
+  shift() {
+    if (this.length < 1) {
+      console.log("There's nothing to remove!");
+      return undefined;
+    }
+    var oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+    return oldHead;
+  }
+
+  unshift(val) {
+    var newNode = new Node(val);
+    if (this.length < 1) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head.prev = newNode;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(idx) {
+    if (idx < 0 || idx >= this.length) return undefined;
+    var current;
+    if (idx > this.length / 2) {
+      let counter = this.length - 1;
+      current = this.tail;
+      while (counter > idx) {
+        current = current.prev;
+        counter--;
+      }
+      return current;
+    } else {
+      let counter = 0;
+      current = this.head;
+      while (counter < idx) {
+        current = current.next;
+        counter++;
+      }
+      return current;
+    }
+  }
+}
