9a5fa23b9f5f9eacba983264af60a305669d3726
Adam <gienappa@gmail.com>
2020-04-25 12:39:10 -0700

    MBH - extractMax revised (fixed behavior when this.storage.length <= 3)

--- data-structures/binaryHeap.js
+++ data-structures/binaryHeap.js
@@ -29,14 +29,27 @@ class MaxBinaryHeap {
   }
 
   extractMax() {
+    if (this.storage.length === 1) {
+      return this.storage.pop();
+    }
+
     let max = this.storage[0];
     this.storage[0] = this.storage.pop();
     let index = 0;
 
-    while( this.storage[2*index + 1]) {
-      let switchIdx = this.storage[2 * index + 1] > this.storage[2 * index + 2] ? 2 * index + 1 : 2 * index + 2;
-      [this.storage[index], this.storage[switchIdx]] = [this.storage[switchIdx], this.storage[index]];
-      index = switchIdx;
+    while( 2 * index + 1 < this.storage.length) {
+      if (2 * index + 2 >= this.storage.length) {
+        if (this.storage[2 * index + 1] > this.storage[index]) {
+          [this.storage[index], this.storage[2 * index + 1]] = [this.storage[2 * index + 1], this.storage[index]];
+          index = 2 * index + 1;
+        } else {
+          break;
+        }
+      } else {
+        let switchIdx = this.storage[2 * index + 1] > this.storage[2 * index + 2] ? 2 * index + 1 : 2 * index + 2;
+        [this.storage[index], this.storage[switchIdx]] = [this.storage[switchIdx], this.storage[index]];
+        index = switchIdx;
+      }
     }
 
     return max;
