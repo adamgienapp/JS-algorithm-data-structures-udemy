9cb9d7a7e04681283358a45023d572de96321e78
Adam <gienappa@gmail.com>
2020-04-26 12:39:17 -0700

    comments added for hashing function

--- data-structures/hashTable.js
+++ data-structures/hashTable.js
@@ -1,10 +1,13 @@
+// arrayLen to be prime - better distribution
+// for inner loop, # of iterations capped at 100 to cover time of very long key
 function hash(key, arrayLen) {
   let total = 0;
   let prime = 31;
-  for (let i = 0; i < Math.min(ley.length, 100); i++) {
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
     let char = key[i];
     let val = char.charCodeAt(0) - 96;
     total = (total * prime + val) % arrayLen;
   }
   return total;
 }
+
