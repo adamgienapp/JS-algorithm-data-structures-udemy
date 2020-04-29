39aeef5a24603c457773b94bd49de49885a8cdbc
Adam <gienappa@gmail.com>
2020-04-29 13:32:58 -0700

    HashTable - get and set complete

--- data-structures/hashTable.js
+++ data-structures/hashTable.js
@@ -1,13 +1,45 @@
-// arrayLen to be prime - better distribution
-// for inner loop, # of iterations capped at 100 to cover time of very long key
-function hash(key, arrayLen) {
-  let total = 0;
-  let prime = 31;
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i];
-    let val = char.charCodeAt(0) - 96;
-    total = (total * prime + val) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
-  return total;
-}
 
+  _hash(key) {
+    let total = 0;
+    let prime = 31;
+    // # of iterations capped at 100 to cover time of very long key
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let val = char.charCodeAt(0) - 96;
+      total = (total * prime + val) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, val) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) {
+          this.keyMap[index][i][1] = val;
+          return;
+        }
+      }
+      this.keyMap[index].push([key, val]);
+    } else {
+      this.keyMap[index] = [ [key, val] ];
+    }
+  }
+
+  get(key) {
+    let index = this._hash(key);
+    let bucket = this.keyMap[index];
+    if (bucket) {
+      for(let tuple of bucket) {
+        if (tuple[0] === key) {
+          return tuple;
+        }
+      }
+    }
+    return undefined;
+  }
+}
